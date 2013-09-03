d910c8f9b61bb0f04076f1037adc3ab95fbcc30c
Alex Peters <lxp@cpan.org>
2013-09-03 23:04:55 +1000

    MooseX::Types 0.22 or greater

--- lib/Finance/Bank/Bankwest/Session.pm
+++ lib/Finance/Bank/Bankwest/Session.pm
@@ -55,7 +55,7 @@ class Finance::Bank::Bankwest::Session {
 
     use Finance::Bank::Bankwest::Parsers ();
     use MooseX::StrictConstructor; # no exports
-    use MooseX::Types; # for "class_type"
+    use MooseX::Types 0.22; # for "class_type"; 0.22 needed by MooseX::Types::Structured
     use TryCatch 1.003002; # for "try" and "catch"; oldest for Travis CI
 
     # Allow instantiation via ->new($mech).
